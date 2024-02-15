ae856a74bad8fc8e81a4a46787461eccaca26740
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-15 10:31:25 -0300

    Index listo

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,9 @@
-//soy el index
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+
+// Syncing all the models at once.
+conn.sync({ force: false}).then(() => {
+  server.listen(3001, () => {
+    console.log("Listening at 3001");
+  });
+});
