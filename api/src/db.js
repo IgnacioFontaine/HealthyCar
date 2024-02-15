55995d122bf08dc316f0a64063d08c09290aab68
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-15 10:32:51 -0300

    bd lista

--- api/src/db.js
+++ api/src/db.js
@@ -1,1 +1,49 @@
-// soy la db
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+
+
+//Conección
+const sequelize = new Sequelize(
+  `postgres://postgres:Admin@localhost/healthyCar`,
+  {
+    logging: false,
+    native: false,
+  }
+);
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+  });
+
+// Injectamos la conexion (sequelize) a todos los modelos
+modelDefiners.forEach((model) => model(sequelize));
+// Capitalizamos los nombres de los modelos ie: product => Product
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+// En sequelize.models están todos los modelos importados como propiedades
+// Para relacionarlos hacemos un destructuring
+const { User, Vehicle  } = sequelize.models;
+
+// Relaciones
+Vehicle.belongsTo(User);
+User.hasMany(Vehicle);
+
+module.exports = {
+  ...sequelize.models,
+  conn: sequelize,
+};
