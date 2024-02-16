e21263f6e6eaa6c543e9d262a161406e1ccd1793
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-16 11:32:37 -0300

    importaciones listas

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home"
+import Error from "./Components/Error"
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />}  />
+      </Routes>
+    </div>
   )
 }
 
