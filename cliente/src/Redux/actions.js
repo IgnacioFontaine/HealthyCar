e9ce9ce8857b7e5f460c14f23267cebad328a9be
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-19 10:58:39 -0300

    get type vehicules

--- cliente/src/Redux/actions.js
+++ cliente/src/Redux/actions.js
@@ -2,6 +2,11 @@ import axios from "axios";
 import ACTION_TYPES from './actionTypes'
 
 //User actions
+export const createUser = (user) => async (dispatch) => {
+  const newUser = await axios.post("http://localhost:3001/user/create", user);
+  return dispatch({ type: ACTION_TYPES.CREATE_USER, payload: newUser.data });
+};
+
 export const getAllUsers = () => async (dispatch) => {
   try {
     let result = await axios.get("http://localhost:3001/user");
@@ -59,14 +64,12 @@ export const deleteUser = (id) => {
 
 
 
-export const createUser = (user) => async (dispatch) => {
-  const newUser = await axios.post("http://localhost:3001/user/create", user);
-  return dispatch({ type: ACTION_TYPES.CREATE_USER, payload: newUser.data });
+//Vehicules actions
+export const createVehicule = (vehicule) => async (dispatch) => {
+  const newVehicule = await axios.post("http://localhost:3001/vehicule/create", vehicule);
+  return dispatch({ type: ACTION_TYPES.CREATE_VEHICULE, payload: newVehicule.data });
 };
 
-
-
-//Vehicules actions
 export const getAllVehicules = () => async (dispatch) => {
   try {
     let result = await axios.get("http://localhost:3001/vehicules");
@@ -76,7 +79,12 @@ export const getAllVehicules = () => async (dispatch) => {
   }
 };
 
-export const createVehicule = (vehicule) => async (dispatch) => {
-  const newVehicule = await axios.post("http://localhost:3001/vehicule/create", vehicule);
-  return dispatch({ type: ACTION_TYPES.CREATE_VEHICULE, payload: newVehicule.data });
+export const getTypeVehicules = (type) => async (dispatch) => {
+  try {
+    let result = await axios.get("http://localhost:3001/vehicules", type);
+    return dispatch({ type: ACTION_TYPES.GET_TYPE_VEHICULES, payload: result.data });
+  } catch (error) {
+    return dispatch({ type: ACTION_TYPES.ERROR, payload: error });
+  }
 };
+
