65868ecefc4de143ec414df5dc7395d54928f7f1
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-19 10:50:19 -0300

    get all users

--- cliente/src/Redux/actions.js
+++ cliente/src/Redux/actions.js
@@ -1,6 +1,15 @@
 import axios from "axios";
 import ACTION_TYPES from './actionTypes'
 
+export const getAllUsers = () => async (dispatch) => {
+  try {
+    let result = await axios.get("http://localhost:3001/user");
+    return dispatch({ type: ACTION_TYPES.GET_ALL_USERS, payload: result.data });
+  } catch (error) {
+    return dispatch({ type: ACTION_TYPES.ERROR, payload: error });
+  }
+};
+
 export const getUserName = (name) => async (dispatch) => {
   try {
     let result = await axios.get("http://localhost:3001/user", name);
@@ -26,6 +35,6 @@ export const getAllVehicules = () => async (dispatch) => {
 };
 
 export const createVehicule = (vehicule) => async (dispatch) => {
-  const newVehicule = await axios.post("http://localhost:3001/vehicule", vehicule);
+  const newVehicule = await axios.post("http://localhost:3001/vehicule/create", vehicule);
   return dispatch({ type: ACTION_TYPES.CREATE_VEHICULE, payload: newVehicule.data });
 };
