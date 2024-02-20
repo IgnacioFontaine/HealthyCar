9947128ff3696b5589841f7d86b02fdfa4eb685d
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-20 10:32:08 -0300

    reducer en proceso

--- cliente/src/Redux/reducer.js
+++ cliente/src/Redux/reducer.js
@@ -3,12 +3,19 @@ import ACTION_TYPES from './actionTypes'
 //Config initialState
 const initialState = {
   sesion_user: [],
-  vehicules_user:[],
+  vehicules_user: [],
+  name_vehicules: [],
+  type_vehicules:[],
 };
 
 //Config reducer
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ACTION_TYPES.GET_USER_NAME:
+      return {
+        ...state,
+        sesion_user: action.payload,
+      }
 
     default:
       return {
