354d766448045a772b9119bf410c6d7b3ac6f5f0
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-21 09:48:41 -0300

    cambio app

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -10,8 +10,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<Error />}  />
       </Routes>
     </div>
