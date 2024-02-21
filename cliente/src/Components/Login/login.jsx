10222bfe60c00cc719b1a38f0c8dfc07f6cd8dc9
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-21 10:57:36 -0300

    cambios login

--- cliente/src/Components/Login/login.jsx
+++ cliente/src/Components/Login/login.jsx
@@ -1,20 +1,21 @@
+import './login.css'
 const Login = () => {
 
   return (
-    <div>
+    <div className='container text-center rectangulo d-flex align-items-center'>
       <div>
-        <h1>HealthyCar</h1>
+        <h1 >HealthyCar</h1>
       </div>
-      <div>
-        <div>
-          <h2>User</h2>
+      <div className="container align-content-center ">
+        <div className='seccion_form'>
+          <h2>User: </h2>
           <input aria-label=" You User..."></input>
         </div>
-        <div>
-          <h2>Password</h2>
+        <div className='seccion_form'>
+          <h2>Password:</h2>
           <input type="password" aria-label=" You Password..."></input>
         </div>
-        <div>
+        <div className="container">
           <button>Login</button>
           <button>Recover password</button>
         </div>
