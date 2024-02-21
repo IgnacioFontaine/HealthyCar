823d9bbb824584fc94a0959cf2bbd6e4442c9e73
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-21 11:16:51 -0300

    componentes en proceso

--- cliente/src/Components/NewVehicule/newVehicule.jsx
+++ cliente/src/Components/NewVehicule/newVehicule.jsx
@@ -7,8 +7,13 @@ const NewVehicules = () => {
           <h1 >Form creación de vehículo</h1>
         </div>
         <div>
-          <h2>Vehiculo 1</h2>
-          <h2>Vehiculo 2</h2>
+          <input placeholder="Name..."></input>
+          <select placeholder="Type...">
+            <menuitem>Car</menuitem>
+            <menuitem>Van</menuitem>
+            <menuitem>Motorcycle</menuitem>
+            <menuitem>Another</menuitem>
+          </select>
         </div>
       </div>
     </div>
