38178f529aab7264ae7e2bd46612520cf0aa4441
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-21 09:32:32 -0300

    login en proceso

--- cliente/src/Components/Login/login.jsx
+++ cliente/src/Components/Login/login.jsx
@@ -1,8 +1,24 @@
 const Login = () => {
 
   return (
-    < div>
+    <div>
+      <div>
         <h1>Soy el Login</h1>
+      </div>
+      <div>
+        <div>
+          <h2>User</h2>
+          <input aria-label=" You User..."></input>
+        </div>
+        <div>
+          <h2>Password</h2>
+          <input type="password" aria-label=" You Password..."></input>
+        </div>
+        <div>
+          <button>Login</button>
+          <button>Recover password</button>
+        </div>
+      </div>
     </div>
   );
 };
