7c021d171c3f529b11344abd772975674b63d277
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-21 11:14:04 -0300

    new vehiculo en proceso

---
+++ cliente/src/Components/NewVehicule/newVehicule.jsx
@@ -0,0 +1,18 @@
+const NewVehicules = () => {
+
+  return (
+    < div>
+        <div >
+        <div>
+          <h1 >Form creación de vehículo</h1>
+        </div>
+        <div>
+          <h2>Vehiculo 1</h2>
+          <h2>Vehiculo 2</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewVehicules;
