f3866e30c0d0f54745d707b48d137f93de3790c5
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-21 10:28:15 -0300

    home view en proceso

--- cliente/src/Components/Home/home.jsx
+++ cliente/src/Components/Home/home.jsx
@@ -1,8 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
+
 const Home = () => {
+  const navigate = useNavigate()
 
   return (
     < div>
+      <div>
         <h1>Soy el Home</h1>
+      </div>
+      <div >
+        <button onClick={() => navigate("/login")} >Login</button>
+        <button onClick={() => navigate("/lasd")} >Error</button>
+      </div>
     </div>
   );
 };
