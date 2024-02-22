e12a7a47b6937fc6ce45aab6153c135ec41a9d02
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-22 11:42:55 -0300

    cambio en app

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -3,12 +3,16 @@ import { Route, Routes, useLocation  } from "react-router-dom";
 import Login from "./Components/Login/login"
 import Home from "./Components/Home/home"
 import Error from "./Components/Error/error"
+import NavBar from "./Components/NavBar/navBar"
 
 function App() {
-  
+  const location = useLocation()
+  const {pathname} = location
+  const showNavBar = pathname !== '/login' 
 
   return (
     <div className="App">
+      {showNavBar && <NavBar />}
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
@@ -18,4 +22,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
