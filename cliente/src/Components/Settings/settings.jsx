bd3701b2b80a1704d7493b2e9fdca28ec1ed23ce
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-22 11:27:28 -0300

    settings en proceso

--- cliente/src/Components/Settings/settings.jsx
+++ cliente/src/Components/Settings/settings.jsx
@@ -1,17 +1,11 @@
-import { useNavigate } from 'react-router-dom';
 
 const Settings = () => {
-  const navigate = useNavigate()
 
   return (
     < div>
         <div >
         <div>
           <h1 >Error 404</h1>
-        </div>
-        <div >
-            <h3  color={"black"}>Return to Home</h3>
-            <button onClick={()=>navigate("/")} >Home</button>
         </div>
       </div>
     </div>
