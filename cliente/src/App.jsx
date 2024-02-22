0e2d200a9adba322e6a9da520b0ca08f64d23dee
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-22 11:40:42 -0300

    colocando navbar

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation  } from "react-router-dom";
 import Login from "./Components/Login/login"
 import Home from "./Components/Home/home"
 import Error from "./Components/Error/error"
 
 function App() {
-
+  
 
   return (
     <div className="App">
