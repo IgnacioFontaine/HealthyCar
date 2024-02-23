322902854096e98309e5340e2b15dfa45c3ee530
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-23 11:24:55 -0300

    cambios en create account

--- cliente/src/Components/CreateAccount/createAccount.jsx
+++ cliente/src/Components/CreateAccount/createAccount.jsx
@@ -1,8 +1,6 @@
 import './createAccount.css'
-import { useNavigate } from 'react-router-dom';
 
 const CreateAccount= () => {
-  const navigate = useNavigate();
   return (
     <div className='container text-center rectangulo d-flex align-items-center'>
       <div>
@@ -11,15 +9,15 @@ const CreateAccount= () => {
       <div className="container align-content-center ">
         <form className="row g-3">
           <div className="col-auto">
-            <label className="staticEmail2" >Email</label>
-            <input type="text"  className="form-control-plaintext" id="staticEmail2" value="email@example.com"></input>
+            <label htmlFor="staticEmail2" className="visually-hidden">Email</label>
+            <input type="text" readOnly className="form-control-plaintext" id="staticEmail2" value="email@example.com"></input>
           </div>
           <div className="col-auto">
-            <label className="inputPassword2" >Password</label>
+            <label htmlFor="inputPassword2" className="visually-hidden">Password</label>
             <input type="password" className="form-control" id="inputPassword2" placeholder="Password"></input>
           </div>
           <div className="col-auto">
-            <button type="submit" className="btn btn-primary mb-3">Create Account</button>
+            <button type="submit" className="btn btn-primary mb-3">Confirm identity</button>
           </div>
         </form>
       </div>
