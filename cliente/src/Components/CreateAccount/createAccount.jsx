cf290e4b10d2264b237d4e6ac737d07934d4f5ab
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-23 11:05:05 -0300

    create account en proceso

--- cliente/src/Components/CreateAccount/createAccount.jsx
+++ cliente/src/Components/CreateAccount/createAccount.jsx
@@ -9,19 +9,6 @@ const CreateAccount= () => {
         <h1 >HealthyCar</h1>
       </div>
       <div className="container align-content-center ">
-        <div className='seccion_form'>
-          <h2>User: </h2>
-          <input aria-label=" You User..."></input>
-        </div>
-        <div className='seccion_form'>
-          <h2>Password:</h2>
-          <input type="password" aria-label=" You Password..."></input>
-        </div>
-        <div className="container">
-          <button onClick={() => navigate("/home")}>Login</button>
-          <button>Create account</button>
-          <button>Recover password</button>
-        </div>
       </div>
     </div>
   );
