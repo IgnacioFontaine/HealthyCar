e889e36447a5f903b0fc738ba129c9722b0b6c6f
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-23 11:44:20 -0300

    cambios creación vehicule

--- cliente/src/Components/NewVehicule/newVehicule.jsx
+++ cliente/src/Components/NewVehicule/newVehicule.jsx
@@ -7,14 +7,24 @@ const NewVehicules = () => {
           <h1 >Create your vehicule</h1>
         </div>
         <div>
-          <label htmlFor="exampleDataList" className="form-label">Type Vehicule</label>
-          <input className="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search..."></input>
-          <datalist id="datalistOptions">
-            <option name="Car" value="Car"></option>
-            <option name="Motorcycle" value="Motorcycle"></option>
-            <option name="Van" value="Van"></option>
-            <option name="Another" value="Another"></option>
-          </datalist>
+          <div>
+            <label htmlFor="vehiculeName" className="input-label">Name:</label>
+            <input  id="vehiculeName" placeholder="Type name..."></input>
+          </div>
+          <div>
+            <label htmlFor="exampleDataList" className="form-label">Type Vehicule</label>
+            <input className="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search..."></input>
+            <datalist id="datalistOptions">
+              <option name="Car" value="Car"></option>
+              <option name="Motorcycle" value="Motorcycle"></option>
+              <option name="Van" value="Van"></option>
+              <option name="Another" value="Another"></option>
+            </datalist>
+          </div>
+          <div>
+            <label htmlFor="vehiculeName" className="input-label">Name:</label>
+            <input  id="vehiculeName" placeholder="Type name..."></input>
+          </div>
         </div>
       </div>
     </div>
