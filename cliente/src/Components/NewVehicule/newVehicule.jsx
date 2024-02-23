25ede2596ffbc537f7c51575cd9b98da106c12a6
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-23 11:58:34 -0300

    cambio new vehicule

--- cliente/src/Components/NewVehicule/newVehicule.jsx
+++ cliente/src/Components/NewVehicule/newVehicule.jsx
@@ -1,18 +1,20 @@
+import './newVehicule.css';
+
 const NewVehicules = () => {
 
   return (
     < div className=" d-flex flex-column justify-content-center align-items-center">
         <div >
         <div>
-          <h1 >Create your vehicule</h1>
+          <h1 className="titulo align-text-center" >Create your vehicule</h1>
         </div>
-        <div className="container">
-          <div>
+        <div className="container form-create">
+          <div className="flex input-vehicule">
             <label htmlFor="vehiculeName" className="input-label">Name:</label>
             <input  id="vehiculeName" placeholder="Type name..."></input>
           </div>
           <div>
-            <label htmlFor="exampleDataList" className="form-label">Type Vehicule</label>
+            <label htmlFor="exampleDataList" className="form-label">Select</label>
             <input className="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search..."></input>
             <datalist id="datalistOptions">
               <option name="Car" value="Car"></option>
@@ -21,15 +23,15 @@ const NewVehicules = () => {
               <option name="Other" value="Other"></option>
             </datalist>
           </div>
-          <div>
+          <div className="flex input-vehicule">
             <label htmlFor="vehiculeTotalKm" className="input-label">Total Km:</label>
             <input  id="vehiculeTotalKm" placeholder=""></input>
           </div>
-          <div>
+          <div className="flex input-vehicule">
             <label htmlFor="vehiculeOilKm" className="input-label">Oil Km:</label>
             <input  id="vehiculeOilKm" placeholder=""></input>
           </div>
-          <div>
+          <div className="flex input-vehicule">
             <label htmlFor="vehiculeServiceKm" className="input-label">Service Km:</label>
             <input  id="vehiculeServiceKm" placeholder=""></input>
           </div>
