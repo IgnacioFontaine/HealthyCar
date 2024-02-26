2b6e64c8d5eb88d8dfa97c092b23aebc005ea608
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-02-26 11:11:50 -0300

    create accoount en proceso

--- cliente/src/Components/CreateAccount/createAccount.jsx
+++ cliente/src/Components/CreateAccount/createAccount.jsx
@@ -6,9 +6,12 @@ const CreateAccount = () => {
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
-    name: "",
+    email: "",
     passwrd: ""
   });
+
+  const handleChange = (event) =>
+    setFormData({ ...formData, [event.target.name]: event.target.value });
 
   return (
     <div className='container text-center rectangulo d-flex align-items-center'>
@@ -19,7 +22,7 @@ const CreateAccount = () => {
         <form className="row g-3">
           <div className="col-auto">
             <label htmlFor="staticEmail2" className="visually-hidden">Email</label>
-            <input type="text" readOnly className="form-control-plaintext" id="staticEmail2" value="email@example.com"></input>
+            <input type="text" readOnly className="form-control-plaintext" id="staticEmail2" value={formData.email} placeholder='email@example.com' ></input>
           </div>
           <div className="col-auto">
             <label htmlFor="inputPassword2" className="visually-hidden">Password</label>
